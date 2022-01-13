063833684555f74daf46b74c06f49ea3bc55dd44
Maciej Ziółkowski <maciej.macron.ziolkowski@gmail.com>
2022-01-13 23:00:45 +0100

    Added images and links!

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description='Run streamlit server')
 parser.add_argument('--nonlp-rep-path', default='./names_and_reps-nonlp.pkl')
 parser.add_argument('--nlp-rep-path', default='./names_and_reps-nlp.pkl')
 parser.add_argument('--cbog-rep-path', default='./names_and_reps-cbog.pkl')
+parser.add_argument('--fluff-data-path', default='./fluff_data.csv')
 args = parser.parse_args()
 
 @st.cache(suppress_st_warning=True)
@@ -18,6 +19,11 @@ def load_cached_data(model):
         return load_data(args.cbog_rep_path)
     return load_data(args.nonlp_rep_path)
 
+@st.cache(suppress_st_warning=True)
+def get_fluff_df():
+    return pd.read_csv('fluff_data.csv')
+
+fluff = get_fluff_df()
 st.header("Recomendation engine for games")
 model = st.radio("Choose model for representations", ["No NLP", "NLP", "CBoG"])
 data = load_cached_data(model)
@@ -41,8 +47,14 @@ number = st.number_input('Select number of recomendations:', 0,50,10)
 st.write('Show top',number,' recomendations')
 
 if game_1 and game_2 and weight and number and st.button('Search'):
-    recomendations = get_games(game_1, game_2, weight, data, number)
-    st.write("TOP{} recomendations score smaller the better".format(number))
-    st.write(pd.DataFrame(recomendations, columns=['Game name','Score']))
+    recomendations = pd.DataFrame(get_games(game_1, game_2, weight, data, number), columns=['Game name','Score'])
+    st.header("TOP {} recomendations - the smaller score, the better!".format(number))
+    recomendations_fluffed = recomendations.merge(fluff, how="left", left_on='Game name', right_on='name')[['Game name', 'Score', 'url', 'img_url']]
+    for i, (index, rec) in enumerate(recomendations_fluffed.iterrows()):
+    	col1, col2, col3 = st.columns([1, 6, 3])
+    	col1.subheader(f"{i+1}")
+    	col2.subheader(f"{rec['Game name']}, score: {rec['Score']:.3f}! [Store link!]({rec['url']})")
+    	col3.image(f"{rec['img_url']}")
+    	
 
 
